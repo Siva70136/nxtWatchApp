adba4de9055def3b74d648fffbb3cb48ecca00f4
root <cheerlasiva143@gmail.com>
2023-03-05 14:20:11 +0000

    insert 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,50 @@
 import {Link} from 'react-router-dom'
+import {AiFillHome, AiFillFire} from 'react-icons/ai'
+import {
+  NavItemContainer,
+  Item,
+  Caption,
+  MainContainer,
+} from './styledComponents'
 import './index.css'
 
 const Menu = () => (
-  <div className="main-menu-container">
-    <div className="menu-container">
+  <>
+    <MainContainer>
       <Link to="/" className="nav-link active-tab">
-        <li className="item">Home</li>
+        <Item>
+          <NavItemContainer>
+            <AiFillHome />
+            <Caption className=""> Home</Caption>
+          </NavItemContainer>
+        </Item>
       </Link>
-    </div>
-  </div>
+      <Link to="/trending" className="nav-link active-tab">
+        <Item>
+          <NavItemContainer>
+            <AiFillFire />
+            <Caption className=""> Trending</Caption>
+          </NavItemContainer>
+        </Item>
+      </Link>
+      <Link to="/games" className="nav-link active-tab">
+        <Item>
+          <NavItemContainer>
+            <AiFillHome />
+            <Caption className=""> Games</Caption>
+          </NavItemContainer>
+        </Item>
+      </Link>
+      <Link to="/save" className="nav-link active-tab">
+        <Item>
+          <NavItemContainer>
+            <AiFillHome />
+            <Caption className=""> Saved Videos</Caption>
+          </NavItemContainer>
+        </Item>
+      </Link>
+    </MainContainer>
+  </>
 )
 
 export default Menu
---
+++ src/components/Menu/styledComponents.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  padding: 10px;
+`
+
+export const Item = styled.li`
+  list-style: none;
+`
+export const NavItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100px;
+`
+
+export const Caption = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+`
